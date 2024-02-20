42595db0f18bee9d9b8979c89fd1af84eb645793
OMO-git <omogit3@gmail.com>
2024-02-21 02:52:02 +0700

    refactor: init project

---
+++ src/app/components/data.js
@@ -0,0 +1,44 @@
+export const platforms = [
+  {
+    _id: "1",
+    name: "Youtube Premium",
+    logoUrl: "https://seakun.id/images/icons/youtube.svg",
+    packages: [
+      {
+        _id: "1a",
+        name: "user regular",
+        price: 28000,
+        durations: [
+          {
+            _id: "a",
+            month: 1,
+          },
+          {
+            _id: "b",
+            month: 3,
+          },
+          {
+            _id: "c",
+            month: 6,
+          },
+          {
+            _id: "d",
+            month: 12,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "2",
+    name: "Spotify",
+    logoUrl: "https://seakun.id/images/icons/youtube.svg",
+    price: 28000,
+  },
+  {
+    _id: "3",
+    name: "Youtube Premium",
+    logoUrl: "https://seakun.id/images/icons/youtube.svg",
+    price: 28000,
+  },
+];
